90c66fd59be48312cd1739df25b80292d7188756
João Neves <joaompnevez@gmail.com>
2015-10-25 19:26:35 +0000

    Add samples

---
+++ samples/Canvas.ts
@@ -0,0 +1,6 @@
+import { Rectangle, Circle, Triangle, Shape } from "Shapes"
+
+export class Canvas {
+	public drawShapes(shapes: Shape[]) { } 
+	public clear() { }
+}
---
+++ samples/Shapes.ts
@@ -0,0 +1,24 @@
+
+export class Shape {
+	public draw() {}
+	public resize() {}
+	public rotate() { }
+}
+
+export class Triangle extends Shape {
+	public draw() { }
+}
+
+export class Rectangle extends Shape {
+	public draw() { }
+	public get width(): number { return 0; }
+	public set width(w: number) { }
+	public get height(): number { return 0; }
+	public set height(h: number) { }
+}
+
+export class Circle extends Shape {
+	public draw() { }
+	public get radius(): number { return 0; }
+	public set radius(r: number) { }
+}
